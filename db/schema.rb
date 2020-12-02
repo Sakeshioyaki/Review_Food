e86ba1ecef0ab98205e3d93db50ed649a7b328b8
Ubuntu <ubuntu@ip-172-31-28-27.ec2.internal>
2020-12-02 11:18:40 +0000

    Add restautant

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_02_103922) do
+ActiveRecord::Schema.define(version: 2020_12_02_111657) do
+
+  create_table "restaurants", force: :cascade do |t|
+    t.string "restaurant_code"
+    t.string "name"
+    t.string "address"
+    t.string "description"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
