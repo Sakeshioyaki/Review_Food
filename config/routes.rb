42a04680b9e972b62aa1ffafbe1c817c3d68d966
Sakeshioyaki <>
2020-12-03 18:55:57 +0000

    fix confix

--- config/routes.rb
+++ config/routes.rb
@@ -4,9 +4,9 @@ Rails.application.routes.draw do
   get 'users/show'
   get 'static_pages/home'
   get '/', to: redirect('/static_pages/home')
-  resources :restaurant_reviews
+  resources :reviews
+  post 'restaurant_review/create', to: 'restaurant_review#create'
   post 'restaurants/:restaurant_id/update_address', to: 'restaurant#update_address'
-  # post 'restaurant_review/create', to: 'restaurant_reviews#create'
-  devise_for :users
+  devise_for :users, controllers: { sessions: 'users/sessions' }
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
